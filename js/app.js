d217105b947a5652128537f641ac5e3acfdc2f70
Whymess <Jonathan.lev.messing@gmail.com>
2016-07-24 19:23:18 -0400

    alert box

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@ $(function(){
 
 
 	$('img').bind('click', function(){
-
 		var initial = this.id
 		if(typeof $(this).data('counter') == 'undefined'){
 			$(this).data('counter', 1);
-			console.log($(this).data('counter') + initial)
+			alert($(this).data('counter') + ' ' + initial)
 		} else {
 			$(this).data('counter', $(this).data('counter')+1)
-			console.log($(this).data('counter') + initial)
+			alert($(this).data('counter') + ' ' + initial)
+
 		}
 	})
 
