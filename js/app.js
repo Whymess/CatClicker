93bbd5d68ffd90f7fa3db5c5de2d3fdeb6067016
Whymess <Jonathan.lev.messing@gmail.com>
2016-07-24 19:18:49 -0400

    Done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
 'use strict'; 
 
+$(function(){
+
+
+	$('img').bind('click', function(){
+
+		var initial = this.id
+		if(typeof $(this).data('counter') == 'undefined'){
+			$(this).data('counter', 1);
+			console.log($(this).data('counter') + initial)
+		} else {
+			$(this).data('counter', $(this).data('counter')+1)
+			console.log($(this).data('counter') + initial)
+		}
+	})
+
+
+});
+
+
+
+
+
+
+
+
+
+
