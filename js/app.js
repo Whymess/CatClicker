4a7d7da5f0eff44f7cb2148d4331626cf3d3c84b
Whymess <Jonathan.lev.messing@gmail.com>
2016-07-24 14:20:11 -0400

    added jquery

--- js/app.js
+++ js/app.js
@@ -1,15 +1,2 @@
 'use strict'; 
 
-(function(){
-	function getImage(){
-		var catImage = document.getElementById('cat');
-		var counter = 0;
-		catImage.addEventListener('click', function(){
-			counter++;
-  			document.getElementById('counter').innerHTML = counter
-  		}, false);
-	}
-
-
-	getImage()
-})()
